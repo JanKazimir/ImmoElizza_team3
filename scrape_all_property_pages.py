7fb9b544b6edd1da28008446267c34b83945b34b
Jkzmr <96251811+JanKazimir@users.noreply.github.com>
2026-02-26 12:33:11 +0100

    scrape all property not working with dict yet, but close

--- scrape_all_property_pages.py
+++ scrape_all_property_pages.py
@@ -13,27 +13,63 @@ from scrape_a_property_page import scrape_data_from_property_page
 ## This is where we write the funciton that call on  scrape_data_from_property_page() to scrape all the page:
 # What we need to do 
 # we get a list. either as a csv or as a list. 
-# create a session and pass that to the scrape data
-
-## What we need to give : a url, a target file, a page index, and a session (to be implemented)
 
 # let's do the easy thing: make a list of three urls, and get that to work with a session
 
 # Add exception handling
 
+# scrape_data_from_property_page needs: (url, page_index, session, target_path)
 
-def scrape_all_property_pages(target_path="testing_scrape_a_page.jsonl"):
+def open_json_and_put_links_in_dict(source_file):
+    with open(source_file, "r") as f:
+        data = json.load(f)
+        links_as_dict = {}
+        
+        for item in data:
+            links_as_dict[item["id"]] = links_as_dict["id"]
+            links_as_dict[item['url']] = item['url']
+    print(links_as_dict)        
+    return links_as_dict
+        
+
+
+
+def scrape_all_property_pages(source_file="links_for_jan_test.json", target_path="testing_scrape_a_page.jsonl"):
     ## here we get the list
+    source_file = "links_for_jan_test.json"
+    list_of_links = ["https://immovlan.be/en/detail/apartment/for-sale/1050/elsene/vbd91331", "https://immovlan.be/en/detail/apartment/for-sale/5000/namur/vbd89112", "https://immovlan.be/en/detail/residence/for-sale/4000/liege/vbd90637"]
     
-    test_prop_links_list = ["https://immovlan.be/en/detail/apartment/for-sale/1050/elsene/vbd91331", "https://immovlan.be/en/detail/apartment/for-sale/5000/namur/vbd89112", "https://immovlan.be/en/detail/residence/for-sale/4000/liege/vbd90637"]
-    target_path= target_path
+    links_as_dict = open_json_and_put_links_in_dict(source_file)
+    print(links_as_dict)
     
-    # to work from a list. 
-    for index, url in enumerate(test_prop_links_list, start=1):
-        url = url
-        page_index = index
+    for index, url in links_as_dict.items():
+        print(index, url)
+        
         with requests.Session() as session:
-            scrape_data_from_property_page(url, page_index, session, target_path)
+            time.sleep(0.1)
+            print("waiting a bit...")
+            try:
+                scrape_data_from_property_page(url, index, session, target_path)
+            except Exception as e:
+                print(f"Got an error:{e}  on index {index}, continuing...")
+                continue
+        
+""" 
+    # We scrape all the links from a list:
+    for index, url in enumerate(list_of_links, start=1):
+        pass
+        url = url
+        url_index = index
+        with requests.Session() as session:
+            time.sleep(0.1)
+            print("waiting a bit...")
+            try:
+                scrape_data_from_property_page(url, url_index, session, target_path)
+            except Exception as e:
+                print(f"Got an error:{e}  on index {index}, continuing...")
+                continue
+   """          
+    
 
 
 scrape_all_property_pages()
@@ -41,3 +77,10 @@ scrape_all_property_pages()
 ## testing quick: works
 #url = "https://immovlan.be/en/detail/residence/for-sale/2970/schilde/rbv23379"
 #scrape_data_from_property_page(url)
+
+""" 
+            print(
+                f"Scaricando pagina {n}...", end="\r"
+            )  # end='\r' sovrascrive la stessa riga, molto pulito!
+            
+             """
