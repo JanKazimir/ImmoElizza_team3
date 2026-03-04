8ad66262cb70830a1513aa74bfd6f6fda1e19c54
Jkzmr <96251811+JanKazimir@users.noreply.github.com>
2026-03-04 19:26:46 +0100

    wrote some ideas

--- aajan_stuff/scratch.py
+++
@@ -1,9 +0,0 @@
-import time
-from datetime import datetime
-
-start_time = datetime.now()
-print(f"starting at: {datetime.now()}")
-time.sleep(1)
-finished_time = datetime.now()
-print(f"finished at: {datetime.now()}. Total time: {finished_time - start_time}")
-print(datetime.now())
